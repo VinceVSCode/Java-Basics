ea87ab634f3b893d7b05d563f1d3897b0b0443cf
Themis Sarafidis <sarafidis12@gmail.com>
2025-07-17 18:26:57 +0300

    Initial commit

---
+++ basics/HelloWorld.java
@@ -0,0 +1,11 @@
+package basics;
+
+import java.util.logging.Logger;
+
+public class HelloWorld {
+    private static final Logger logger = Logger.getLogger(HelloWorld.class.getName());
+    
+    public static void main(String[] args) {
+        logger.info("Hello, World!");
+    }
+}
