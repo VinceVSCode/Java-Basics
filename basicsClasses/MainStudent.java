d9973481e005f1bd7e429a394cacae78ea96ce1e
Themis Sarafidis <sarafidis12@gmail.com>
2025-07-21 16:04:55 +0300

    Add Student and MainStudent classes to demonstrate student information management and passing status check

---
+++ basicsClasses/MainStudent.java
@@ -0,0 +1,16 @@
+public class MainStudent {
+    public static void main(String[] args) {
+        // Create a new student object
+        Student student1 = new Student(85, "Alice");
+        Student student2 = new Student(45, "Bob");
+
+        // Display student information
+        student1.displayInfo();
+        student2.displayInfo();
+
+        // Check if students are passing
+        student1.isPassing();
+        student2.isPassing();
+    }
+    
+}
